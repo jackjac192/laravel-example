acb24a59b9273dc10b896ef8443c913dd400b8e2
jackjac192 <jackjac192@gmail.com>
2017-10-13 17:03:00 -0500

    Validacion de datos con un objeto request y creacion de un nuevo mensaje en la base de datos

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Message;
+use App\Http\Requests\CreateMessageRequest;
 use Illuminate\Http\Request;
+
 
 class MessagesController extends Controller
 {
@@ -14,15 +16,13 @@ class MessagesController extends Controller
     	]);
     }
 
-    public function create(Request $request){
-    	$this->validate($request,[
-    		'message' => ['required', 'max:140']
-    	],
-    	[
-    		'message.required' => 'Por favor, escribe tu mensaje',
-    		'message.max' => 'El mesnaje no puede superar los 140 caracteres'
-    	]
-    );
-		return 'hola';
+    public function create(CreateMessageRequest $request)
+    {
+    	$message = Message::create([
+    		'content' => $request->input('message'),
+    		'image' => 'http://lorempixel.com/600/338?'.mt_rand(0,1000)
+    	]);
+
+    	return redirect('/messages/'.$message->id);
 	}
-}
+} 
